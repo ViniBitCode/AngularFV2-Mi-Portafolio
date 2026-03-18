3b6343e55731b96dfc8c390819f7adc9c59b4206
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-03-18 18:49:52 -0300

    first commit

---
+++ backend/src/test/java/proyecto/portafolio/PortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.portafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
