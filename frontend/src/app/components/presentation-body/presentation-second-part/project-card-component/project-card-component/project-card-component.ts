74f3534828e140161370d273d0671f6051818144
Facundo Vinitzca <facundo@vinitzca.com.ar>
2026-05-01 14:41:33 -0300

    #9 - Hover sobre proyectos para desc del mismo

---
+++ frontend/src/app/components/presentation-body/presentation-second-part/project-card-component/project-card-component/project-card-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card-component',
+  imports: [],
+  templateUrl: './project-card-component.html',
+  styleUrl: './project-card-component.scss',
+})
+export class ProjectCardComponent {}
